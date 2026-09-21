6ff84316afa40df645b2ffdd2a8532f10ab066af
nu11-and-v01d <null@keyefu.com>
2026-09-21 14:22:14 -0500

    Fix iOS build by including dispatch declarations

--- platform/darwin.c
+++ platform/darwin.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dispatch/dispatch.h>
 #include <CommonCrypto/CommonCrypto.h>
 #include <CommonCrypto/CommonRandom.h>
 #include "debug.h"
